f9c685f150b09b5e2502fd66de8267ebb48fba76
Xinchun Liu <lospringliu@gmail.com>
2018-07-10 10:55:04 -0400

    use multiprocessing for wallet balance

--- jsonstore/management/commands/update_balance.py
+++ jsonstore/management/commands/update_balance.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-import time
+import time, multiprocessing
 from moac.models import Address
 
 class Command(BaseCommand):
@@ -7,9 +7,11 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		self.stdout.write("...update moac balances")
+		def update_balance(address):
+			self.stdout.write(self.style.SUCCESS('\t... updating {} ...'.format(address.display)))
+			address.update_balance()
 		while True:
-			for address in Address.objects.filter(flag_balance=True).order_by('-updated'):
-				address.update_balance()
-				self.stdout.write(self.style.SUCCESS('\t... updated {}'.format(address.display)))
+			with multiprocessing.Pool(3) as pool:
+				list(map(update_balance, Address.objects.filter(flag_balance=True).order_by('-updated')))
 			self.stdout.write(self.style.SUCCESS('updated moac balances'))
 			time.sleep(5)
